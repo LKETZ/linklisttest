e798ab5804923b6e9bc97d3de7777841b7baac6e
LKETZ <159238584+LKETZ@users.noreply.github.com>
2024-03-07 12:06:07 +0000

    second

--- recu.c
+++ recu.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
+return_type 
+{
+
+    
+}
+
 void main()
 {
     int x;
     printf("hello\n");
     printf("hello\n");
     x = strlen("hello");
-    printf("\n%d\n",x);
+    printf("%d\n",x);
 
 }
